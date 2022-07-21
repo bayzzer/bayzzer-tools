7f3f4c2dc6964bf4ec7d9d6873b241d06a7ec7f1
Ismael Flores <bayzzer@outlook.com>
2022-07-21 14:04:11 -0500

    fix:  export ImmutableArray

--- types/immutable.ts
+++ types/immutable.ts
@@ -42,8 +42,8 @@ export type Immutable<T> =
   T extends object ? ImmutableObject<T> :
   T
 
-interface ImmutableArray<T> extends ReadonlyArray<Immutable<T>> {}
+export interface ImmutableArray<T> extends ReadonlyArray<Immutable<T>> {}
 
-type ImmutableObject<T> = {
+export type ImmutableObject<T> = {
   readonly [P in keyof T]: Immutable<T[P]>
 }
